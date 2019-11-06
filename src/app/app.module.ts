a20cf554a53c7c4784e93664132f5a2d7bc596a4
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-06 15:13:51 +0500

    Project5Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,24 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
+import { KontaktiComponent } from './components/kontakti/kontakti.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: '', component:CarComponent},
+  {path: 'about', component:KontaktiComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent
+    CarComponent,
+    KontaktiComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
